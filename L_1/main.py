d4392652be72030d63372a325c669f7ef1709655
KaraRi <kirarichi51@gmail.com>
2023-10-02 15:02:16 +0200

    Задачи с Питоном.

---
+++ L_1/main.py
@@ -0,0 +1,229 @@
+# print(5, 8, 6)
+
+# n = 5 
+# print(n)
+
+# n = None
+# print(n)
+
+# n = 1.89
+# print(n)
+
+# n = "Hello World"
+# print(n)
+
+# n = 'Hello World'
+# print(n)
+
+
+"""
+n = 5
+print(type(n))
+
+n = 'Hello Richi'
+print(type(n))
+
+n = 'Hello\'Richi'
+print(n)
+
+n = 'Hello "Richi"'
+print(n)
+"""
+
+
+# a = 5
+# b = 5.89
+# c = 'Hello'
+
+# print(a, b, c)
+
+# a = 5
+# b = 5.89
+# c = 'Hello'
+
+# print(a,' - ',b,' - ',c)
+
+# a = 5
+# b = 5.89
+# c = 'Hello'
+
+# print(f"{a} - {b} - {c}")
+
+# a = 5
+# b = 5.89
+# c = 'Hello'
+
+# print("{} - {} - {}".format(a, b, c))
+
+
+# print("Введите первое число:")
+# a = input()
+# b = input('Введите второе число: ')
+
+# print(a, ' + ', b, ' = ', a + b)
+
+# c = 5.89
+# print(c)
+# n = int(c)
+# print(n)
+
+# c = 5.89
+# print(c)
+# print(type(c))
+# c = int(c)
+# print(c)
+# print(type(c))
+
+# c = 5.89
+# print(c)
+# print(type(c))
+# c = str(c)
+# print(c + '89')
+# print(type(c))
+
+# c = 1
+# print(c)
+# print(type(c))
+# c = bool(c)
+# print(c)
+# print(type(c))
+
+# print("Введите первое число:")
+# a = int(input())
+# b = int(input('Введите второе число: '))
+
+# print(a, ' + ', b, ' = ', a + b)
+
+
+# a = 5.8955
+# b = 6.34523
+# print(a*b)
+
+# a = 5.8955
+# b = 6.34523
+# print(round(a*b, 3))
+
+# iter = 2
+# iter += 3 # iter = iter + 3
+# iter -= 4 # iter = iter - 4
+# iter *= 5 # iter = iter * 5
+# iter /= 5 # iter = iter / 5
+# iter //= 5 # iter = iter // 5
+# iter %= 5 # iter = iter % 5
+# iter **= 5 # iter = iter ** 5
+
+
+# a = 1 > 4
+# print(a)
+# a = 1 < 4 and 5 > 2
+# print(a)
+# a = 1 == 2
+# print(a)
+# a = 1 != 2
+# print(a)
+# a = 'qwe'
+# b = 'qwe'
+# print(a == b)
+# a = 1 < 3 < 5 < 10
+# print(a)
+
+
+# username = input('Введите имя: ')
+# if username == 'Маша':
+#     print('Ого, Здорова Маха!')
+# elif username == 'Марина':
+#     print('Привет, Маринка!')
+# elif username == 'Ричи':
+#     print('Ричи, ты КОСМОС БРАТ!')
+# else:
+#     print('Привет, ', username)
+
+
+# i = 0
+# while i < 5:
+#     if i == 3:
+#         break
+#     i = i + 1
+# else:
+#     print('Пожалуй')
+#     print('Хватит )')
+# print(i)
+
+# n = int(input())
+# flag = True
+# i = 2
+# while flag:
+#     if n % i == 0: # если остаток при делении числа n на i равен 0
+#         flag = False
+#         print(i)
+#     elif i > n // 2: # делить числа не может превышать введённое число, делённое на 2 
+#         print(n)
+#         flag = False
+#     i += 1
+
+# a = 'qwerty'
+# print(a[2])
+
+# a = 'qwerty'
+
+# for i in a:
+#         print(i)
+
+# line = ""
+# for i in range(5):
+#     line = ""
+#     for j in range(5):
+#         line += "+"
+#     print(line)
+
+# text = 'СкУшАй ЕщЁ БуЛочКу ВнУчЁк!'
+# print(len(text))
+# print('ЕщЁ' in text)
+# print(text.lower())
+# print(text.upper())
+# print(text.replace('ЕщЁ','ещё'))
+
+# text = 'Съешь ещё этих мягких французских булок'
+# print(text[0])                          # с
+# print(text[1])                          # ъ
+# print(text[len(text)-1])                # к
+# print(text[-7])                         # х / Элемент считается с конца массива
+# print(text[-5])                         # б
+# print(text[:])                          # Съешь ещё этих мягких французских булок             
+# print(text[:2])                         # Съ
+# print(text[len(text)-2:])               # ок
+# print(text[2:9])                        # ешь ещё
+# print(text[6:-18])                      # ещё этих мягких
+# print(text[0:len(text):6])              # Сеикакл
+# print(text[::6])                        # Сеикакл
+# text = text[2:9] + text[-5] + text[:2]  # ....
+
+
+
+
+# За день машина проезжает n километров. 
+# Сколько дней нужно, чтобы проехать 
+# маршрут длиной m километров? 
+# При решении этой задачи нельзя пользоваться 
+# условной конструкцией if и циклами.
+# Input:
+# n = 700км/д
+# s= 750км
+# Output:
+# 2
+
+# a = 'Python'
+# b = 'Hello world!'
+# c = 'Привет меня зовут Вася'
+# age = 28
+
+# print(a,b,c, sep='***', end='\n')
+# print()
+# print(a, end='~~~')
+# print(b, end='~~~')
+# print(c)
+# print()
+
+# print(*c, sep='\n')
+# print('П','р', 'и', 'в', 'е', ....')
+
