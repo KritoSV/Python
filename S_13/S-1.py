a1120838f7961adafabf79ebcf4956e6c982da10
KaraRi <kirarichi51@gmail.com>
2023-11-17 21:30:37 +0200

    Задача по аттестации

--- S_13/S-1.py
+++ S_13/S-1.py
@@ -2,14 +2,18 @@
 # которая состоит всего из 1 столбца. 
 # Ваша задача перевести его в one hot вид. 
 # Сможете ли вы это сделать без get_dummies?
-import pandas as pd
+
+
+import pandas as pd 
+import numpy as np 
 import random
+ 
 lst = ['robot'] * 10
 lst += ['human'] * 10
 random.shuffle(lst)
-data = pd.DataFrame({'whoAmI':lst})
+data = pd.DataFrame({'whoAmI': lst})
 data.head()
-
+ 
 print('')
 
 data['tmp'] = 1
